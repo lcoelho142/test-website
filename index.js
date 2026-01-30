9d8bd195a89b4dd6ef644cb11a3a0f728eac7eef
Leah Coelho <lcoelho142@gmail.com>
2026-01-30 11:24:16 -0500

    highlight home button

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// const buttons = document.querySelectorAll("header > button");
+// const validIDs = ["home-01","home-02","home-03","about-01","about-02","about-03",
+//   "contact-01","contact-02","blog-01","blog-02","delete-data-01","delete-data-02",
+//   "privacy-01","privacy-02","terms-01","terms-02"];
+
+// const imgEl = document.getElementById("website-img");
+
+// document.addEventListener("click", (e) => {
+//   if (!validIDs.includes(e.target.id)) return;
+
+//   // Update image
+//   imgEl.src = `./images/${e.target.id}.png`;
+
+//   // Clear previous active
+//   buttons.forEach(btn => btn.classList.remove("active"));
+
+//   // Set new active
+//   e.target.classList.add("active");
+// });
+
 const buttons = document.querySelectorAll("header > button");
 const validIDs = ["home-01","home-02","home-03","about-01","about-02","about-03",
   "contact-01","contact-02","blog-01","blog-02","delete-data-01","delete-data-02",
@@ -5,15 +25,15 @@ const validIDs = ["home-01","home-02","home-03","about-01","about-02","about-03"
 
 const imgEl = document.getElementById("website-img");
 
+// Default on refresh
+const defaultBtn = document.getElementById("home-01");
+defaultBtn.classList.add("active");
+imgEl.src = "./images/home-01.png";
+
 document.addEventListener("click", (e) => {
   if (!validIDs.includes(e.target.id)) return;
 
-  // Update image
   imgEl.src = `./images/${e.target.id}.png`;
-
-  // Clear previous active
-  buttons.forEach(btn => btn.classList.remove("active"));
-
-  // Set new active
+  buttons.forEach(b => b.classList.remove("active"));
   e.target.classList.add("active");
 });
